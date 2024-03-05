83008d60cdf13c0547337cbc022e9dbc691b52ec
Engin Oksuz <38870269+enginksz@users.noreply.github.com>
2024-03-05 23:09:31 +0300

    Update objecttrackingdeepsort.py

--- objecttrackingdeepsort.py
+++ objecttrackingdeepsort.py
@@ -2,23 +2,17 @@ import cv2
 import numpy as np
 import sys
 import glob
-
 import time
 import torch
 
-
-
 class YoloDetector():
-
+    
     def __init__(self, model_name):
-        
         self.model = self.load_model(model_name)
         self.classes = self.model.names
-        #print(self.classes)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print("Using Device: ", self.device)
         
-
     def load_model(self, model_name):
    
         if model_name:
@@ -28,22 +22,19 @@ class YoloDetector():
         return model
 
     def score_frame(self, frame):
-
+        
         self.model.to(self.device)
         downscale_factor = 2
         width = 640
         height = 480
         frame = cv2.resize(frame, (width,height))
         results = self.model(frame)
-       
         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
         
         return labels, cord
 
     def class_to_label(self, x):
-   
         return self.classes[int(x)]
-
 
     def plot_boxes(self, results, frame, height, width, confidence=0.3):
     
@@ -52,7 +43,6 @@ class YoloDetector():
         n                   = len(labels)
         x_shape, y_shape    = width, height
         
-    
     
         for i in range(n):
             row = cord[i]
@@ -71,12 +61,9 @@ class YoloDetector():
                     
                     detections.append(([x1, y1, int(x2-x1), int(y2-y1)], row[4].item(), 'person'))
                 
-        
         return frame, detections
 
-
 cap = cv2.VideoCapture(0)
-
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -86,7 +73,6 @@ detector = YoloDetector(model_name=None)
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-
 
 from deep_sort_realtime.deepsort_tracker import DeepSort
 
@@ -105,20 +91,15 @@ object_tracker = DeepSort(max_age=5,
                 polygon=False,
                 today=None)
 
-
 while cap.isOpened():
 
     succes, img = cap.read()
- 
     start = time.perf_counter()
-    
     results = detector.score_frame(img)
     img, detections = detector.plot_boxes(results, img, height=img.shape[0], width=img.shape[1], confidence=0.5)
-
-        
+  
     tracks = object_tracker.update_tracks(detections, frame=img) # bbs expected to be a list of detections, each in tuples of ( [left,top,w,h], confidence, detection_class )
     
-        
     for track in tracks:
         if not track.is_confirmed():
             continue
@@ -130,12 +111,10 @@ while cap.isOpened():
         cv2.rectangle(img,(int(bbox[0]), int(bbox[1])),(int(bbox[2]), int(bbox[3])),(0,0,255),2)
         cv2.putText(img, "ID: " + str(track_id), (int(bbox[0]), int(bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
     
-    
         
     end = time.perf_counter()
     totalTime = end - start
     fps = 1 / totalTime
-
 
     cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
     cv2.imshow('image',img)
